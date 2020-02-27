67d5155e5290127083d86983d3c65064ef5b7d46
arcum-omni <travis.l.eiler@gmail.com>
2020-02-26 18:08:50 -0800

    Added Privacy View

--- AspnetCoreWithBugs/Controllers/HomeController.cs
+++ AspnetCoreWithBugs/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace AspnetCoreWithBugs.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
